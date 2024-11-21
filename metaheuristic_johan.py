eb02d3e0e98f2de91d4dd31e7076bba91219fc2a
Johsan16 <211240@umons.ac.be>
2024-11-21 18:36:02 +0100

    refactor

--- metaheuristic_johan.py
+++ metaheuristic_johan.py
@@ -16,37 +16,42 @@ def perm(type: int,mat:np.ndarray,index: int):
         mat_tmp[:,index]*=-1
     return mat_tmp
 
-
+def recherche_locale(matrix,pattern,la_totale=False):
+    counter=0
+    while counter<10:
+        for i in range(matrix.shape[0]*matrix.shape[1]):   
+            pattern_tmp=perm(0,pattern,i)
+            if compareP1betterthanP2(matrix,pattern_tmp,pattern):
+                pattern=copy.deepcopy(pattern_tmp)
+                print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
+                counter=0
+        if la_totale:
+            for i in range(matrix.shape[0]):   
+                pattern_tmp=perm(1,pattern,i)
+                if compareP1betterthanP2(matrix,pattern_tmp,pattern):
+                    pattern=copy.deepcopy(pattern_tmp)
+                    print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
+                    counter=0
+            for i in range(matrix.shape[1]):   
+                pattern_tmp=perm(2,pattern,i)
+                if compareP1betterthanP2(matrix,pattern_tmp,pattern):
+                    pattern=copy.deepcopy(pattern_tmp)
+                    print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
+                    counter=0
+        counter+=1
+    return pattern
 
 # matrix=utils.lire_fichier("data/exempleslide_matrice (1).txt")
 # matrix=utils.lire_fichier("data/ledm6_matrice (1).txt")
-matrix=utils.LEDM (5,5)
+matrix=utils.LEDM (25,25)
 print(matrix)
 # pattern=np.random.choice([-1,1],size=matrix.shape)
 pattern=np.ones(matrix.shape)
 # print(pattern)
 print(fobj(matrix,pattern))
-counter=0
-while counter<10:
-    for i in range(matrix.shape[0]*matrix.shape[1]):   
-        pattern_tmp=perm(0,pattern,i)
-        if compareP1betterthanP2(matrix,pattern_tmp,pattern):
-            pattern=copy.deepcopy(pattern_tmp)
-            print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
-            counter=0
-    for i in range(matrix.shape[0]):   
-        pattern_tmp=perm(1,pattern,i)
-        if compareP1betterthanP2(matrix,pattern_tmp,pattern):
-            pattern=copy.deepcopy(pattern_tmp)
-            print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
-            counter=0
-    for i in range(matrix.shape[1]):   
-        pattern_tmp=perm(2,pattern,i)
-        if compareP1betterthanP2(matrix,pattern_tmp,pattern):
-            pattern=copy.deepcopy(pattern_tmp)
-            print(f"rank: {fobj(matrix,pattern)[0]}, valeur min: {fobj(matrix,pattern)[1]}")
-            counter=0
-    counter+=1
+
+print(fobj(matrix,recherche_locale(matrix, pattern,la_totale=False)))
+
 
 utils.ecrire_fichier("solution.txt",matrix,pattern)
 
