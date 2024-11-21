6c4719dd75b0c92d66d8197c433961c11290706e
Medhi VANDERBERKEN <161562256+MedhiVDBK@users.noreply.github.com>
2024-11-21 14:42:56 +0100

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def lire_fichier(file):
+    with open(file, 'r') as f:
+        first_line = f.readline().strip()
+        n_rows, n_cols = map(int, first_line.split())  
+    
+    # Charger le reste du fichier dans un tableau NumPy
+    matrix = np.loadtxt(file, skiprows=1)
+    
+    # Vérification des dimensions
+    if matrix.shape != (n_rows, n_cols):
+        raise ValueError("Les dimensions de la matrice ne correspondent pas à la première ligne.")
+    
+    return matrix
+
+def écrire_fichier(file,matrix):
+    matrix=Transforme_matrix(matrix)
+    with open(file, "w") as f:
+        Sh=matrix.shape
+        for i in range (Sh[0]):
+            for j in range (Sh[1]):
+                f.write(f"{int(matrix[i,j])}")
+                if j!= Sh[1]-1:
+                    f.write(f" ")
+            f.write(f"\n")
+                
+        
+        
+def Transforme_matrix(M):
+    Sh=M.shape
+    for i in range (Sh[0]):
+        for j in range (Sh[1]):
+            if M[i,j]<0:
+                M[i,j]=-1
+            else:
+                M[i,j]=1
+    return M
+        
